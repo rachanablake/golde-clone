8dc3f33e151d5325adbede9719b281dc93dd317c
Rachana Blake <rachanasor222@gmail.com>
2024-01-10 16:25:50 +0700

    Start Clone Golde Websit

---
+++ src/app/layouts/header-layout/header-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-layout.component.html',
+  styleUrl: './header-layout.component.scss'
+})
+export class HeaderLayoutComponent {
+
+}
---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeaderLayoutComponent } from '../../layouts/header-layout/header-layout.component';
+import { FooterLayoutComponent } from '../../layouts/footer-layout/footer-layout.component';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [HeaderLayoutComponent, FooterLayoutComponent],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss'
+})
+export class HomePageComponent {
+
+}
